571500b101a3f00d64067a83058cd97f597e54d8
Jhoel Ordoñez <jhoelordonez2@gmail.com>
2021-01-18 15:51:56 -0500

    Taller

--- Ejercicios/src/ejercicios/Problema01.java
+++ Ejercicios/src/ejercicios/Problema01.java
@@ -16,6 +16,26 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        double[][] resultado = new double[4][2];
+        double x;
+        double[][] datos = {{10, 2}, {1, 5}, {6, 2}, {7, 8}};
+        for (int fila = 0; fila < 4; fila++) {
+            for (int col = 0; col < 2; col++) {
+                x = datos[fila][col];
+                resultado[fila][col] = factoriales(x);
+                System.out.printf("El factorial de: %.2f es: %.2f\n",
+                        datos[fila][col], factoriales(x));
+            }
+        }
+    }
+
+    public static double factoriales(double valor) {
+        if (valor <= 1) {
+            return 1;
+        } else {
+            return valor * factoriales(valor - 1);
+        }
+        
     }
     
 }
